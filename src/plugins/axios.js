b4b0701489d35304dc18796ba66d5e8286feb014
davood <davoodkhany@gmail.com>
2023-04-15 00:46:24 +1000

    Authorization vue js project

--- src/plugins/axios.js
+++ src/plugins/axios.js
@@ -1,13 +1,24 @@
 import axios from "axios";
 
 
+const token = localStorage.getItem('token');
+
+
+
 const axiosInstance = axios.create({
     baseURL: 'http://localhost:8000/api/v1',
     withCredentials: true,
     timeout: 4000,
-    headers: { Accept: 'application/json' }
+    headers: {
+        Accept: 'application/json',
+
+    }
 
 });
+
+
+
+
 
 
 export default axiosInstance;
